b15626ea4701b1ebb257a1828d593517fde2ccbb
moonabys <255314530+moonabys@users.noreply.github.com>
2026-05-10 14:25:54 +0100

    initial commit

---
+++ APTromboneChampMod/Plugin.cs
@@ -0,0 +1,17 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace APTromboneChampMod;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+        
+    private void Awake()
+    {
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_NAME} is running at version {MyPluginInfo.PLUGIN_VERSION}");
+        Logger.LogInfo($"Also Hello World!");
+    }
+}
---
+++ APTromboneChampMod/obj/Debug/netstandard2.0/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace APTromboneChampMod
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "APTromboneChampMod";
+        public const string PLUGIN_NAME = "AP Trombone";
+        public const string PLUGIN_VERSION = "0.0.1";
+    }
+}
